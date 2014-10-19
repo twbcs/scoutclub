9a493f10ca8b983e682ecfed5d50bacf4193af68
Archer Lin <twbcs7@gmail.com>
2014-10-20 07:06:38 +0800

    重構 移除非必要程式碼

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ActiveRecord::Base
 	belongs_to :forum
 	belongs_to :user
+	delegate :name, to: :user, prefix: "post" 
+
 	scope :order_by_updated_at, -> { order(updated_at: :asc) }
 end
