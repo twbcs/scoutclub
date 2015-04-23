a54e5adadaa551ff678f21cae20a1011d06c2ac1
Archer Lin <twbcs7@gmail.com>
2015-04-23 14:18:48 +0800

    fix bug 2

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,3 +7,9 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( photos.js progressbar.gif loading.gif )
+
+config.assets.precompile << Proc.new { |path|
+  if path =~ /\.(eot|svg|ttf|woff)\z/
+    true
+  end
+}
