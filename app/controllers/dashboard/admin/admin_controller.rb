bbb3ca1abf9b76bd96840894123245fb985fcdd6
Archer Lin <twbcs7@gmail.com>
2015-04-30 20:42:36 +0800

    test admin path error 4

--- app/controllers/dashboard/admin/admin_controller.rb
+++ app/controllers/dashboard/admin/admin_controller.rb
@@ -4,8 +4,6 @@ class Dashboard::Admin::AdminController < ApplicationController
 
   def user_rule
     # || forum_admin = Group.find_by(admin: true).
-    unless Modify.find_by(user_id: current_user)
-      redirect_to root_path
-    end
+    redirect_to root_path unless Modify.find_by(user_id: current_user)
   end
 end
