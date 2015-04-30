e1bd86a469a78465a7567ef5166833d811d3757d
Archer Lin <twbcs7@gmail.com>
2015-04-30 17:06:20 +0800

    test admin path error 3

--- app/controllers/dashboard/admin/admin_controller.rb
+++ app/controllers/dashboard/admin/admin_controller.rb
@@ -1,20 +1,11 @@
 class Dashboard::Admin::AdminController < ApplicationController
-  before_action :set_permission
   before_action :user_rule
   layout 'admin'
 
   def user_rule
     # || forum_admin = Group.find_by(admin: true).
-    redirect_to root_path unless @admin == Modify.find_by(user_id: current_user)
-  end
-
-  def set_permission
-    session[:user_level] = UserGroup.where(user_id: current_user.id)
-                           .pluck(:group_id) unless session[:user_level]
-    user_level = session[:user_level]
-    @admin = true if [1, 2, 7].any? { |i| user_level.include?(i) }
-    @superadmin = true if user_level.include?(1)
-    @inside = true if [2, 4, 5, 7].any? { |i| user_level.include?(i) }
-    @junior = true if user_level.include?(3)
+    unless Modify.find_by(user_id: current_user)
+      redirect_to root_path
+    end
   end
 end
