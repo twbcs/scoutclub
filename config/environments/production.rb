a8e05fa1a300e2b4178de65ccbe3e48418b2c0ec
Archer Lin <twbcs7@gmail.com>
2015-06-14 05:22:18 +0800

    try fix robot

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,7 +30,7 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
   config.assets.precompile << /(^[^_\/]|\/[^_])[^\/]*$/
-  config.assets.precompile +=  ['*.js', '*.css', '*.css.erb']
+  config.assets.precompile += ['*.js', '*.css', '*.css.erb']
   # Generate digests for assets URLs.
   config.assets.digest = true
 
